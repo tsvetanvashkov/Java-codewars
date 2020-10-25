cd28e8e90e8939a97a580f72910f394805231881
tsvetanvashkov <tsvetan.vashkov@gmail.com>
2020-10-25 22:48:16 +0200

    Update EqualSidesOfAnArray.java

--- EqualSidesOfAnArray.java
+++ EqualSidesOfAnArray.java
@@ -1,20 +1,5 @@
-package kata;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class EqualSidesOfAnArayy {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int[] input = Arrays.stream(scan.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
-		scan.close();
-		System.out.println(findEvenIndex(input));
-
-	}
-
-
-
- public static int findEvenIndex(int[] arr) {
+public class Kata {
+  public static int findEvenIndex(int[] arr) {
     
     for (int i = 0; i < arr.length; i++) {
       int sumR = 0;
